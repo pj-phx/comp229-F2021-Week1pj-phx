f38bc230cdb9556d29888da0c43114a7ae262901
pj-phx <66885175+pj-phx@users.noreply.github.com>
2021-11-08 12:08:42 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+"use strict"; 
+(function(){
+    function Start(){
+        let myVariable=20;
+        console.log("my number is: "+myVariable);
+    } 
+    window.addEventListener("load",Start);
+
+
+})();
